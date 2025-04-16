cedacc149b96c4864655bedc7589b5b4add818bb
Aditya Pathak <adityap029@gmail.com>
2025-04-16 19:56:38 -0400

    Delete course

--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -11,7 +11,7 @@ export const deleteCourse = async (id: string) => {
   return data;
 };
 export const updateCourse = async (course: any) => {
-  const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
+  const { data } = await axiosWithCredentials.put(`${COURSES_API}/${course._id}`, course);
   return data;
 };
 export const findModulesForCourse = async (courseId: string) => {
